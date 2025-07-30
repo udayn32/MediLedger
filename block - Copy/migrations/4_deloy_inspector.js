02dcafb52378caa978b0548e8ff7ebfc7d5e66d8
Uday Naik <udaynaik057@gmail.com>
2025-07-31 00:21:27 +0530

    Delete block - Copy directory

--- block - Copy/migrations/4_deloy_inspector.js
+++
@@ -1,6 +0,0 @@
-const inspector = artifacts.require("inspector");
-
-module.exports = async function (deployer) {
-    const farmerContractAddress = "0x297fAa5Eca9cFEBE9112588A513e5eCB668ACc17"; // Replace with your Farmer contract address
-    await deployer.deploy(inspector, farmerContractAddress);
-};
